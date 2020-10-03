42f37a59431fdd03b07e27cd243ebee1792ef96e
Roderick Yang <zy7@illinois.edu>
2020-10-02 22:07:09 -0400

    1

--- search_eval.py
+++ search_eval.py
@@ -15,7 +15,7 @@ def load_ranker(cfg_file):
     idx = metapy.index.make_inverted_index('config.toml')
     
     
-    return metapy.index.OkapiBM25(1.2, 0.77, 550)
+    return metapy.index.OkapiBM25(1.2, 0.77, 650)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
